71e656c0051c43189f3c326683122d60d958e03f
edupoly <info.edupoly@gmail.com>
2023-09-20 17:34:41 +0530

    second

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './shared/Header';
+import { Outlet } from 'react-router-dom';
+
+
+function App() {
+  return (
+    
+      <div>
+        <Header></Header>
+        <Outlet></Outlet>
+      </div>
+
+  );
+}
+
+export default App;
---
+++ src/features/AdminDashboad/AddBed.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import { useGetAllHospitalsQuery } from '../../services/hospApi'
+
+function AddBed() {
+    var {isLoading:isHospitalsLoading,data:hopitals}=useGetAllHospitalsQuery();
+    var [newBed,setNewBed]=useState({
+        bedStatus:'open',
+        bedtype:"",
+        bedPrice:0,
+        patients:[]
+    })
+    var [bedTypes,setBedTypes]=useState([])
+    var [selectedHospital,setSlectedHospital]=useState(null)
+    function updateBedTypes(hn){
+      setBedTypes(JSON.parse(hn).bedTypes);
+    }
+  return (
+    <div className='border border-2 border-danger m-2 p-2'>
+        <h1>AddBed</h1>
+        {
+            isHospitalsLoading && (<b>...wait</b>)
+        }
+        {
+            !isHospitalsLoading && (
+                <select onChange={(e)=>{updateBedTypes(e.target.value)}}>
+                    <option value={null} disabled selected> Please select </option>
+                    {
+                        hopitals.map((h)=>{
+                            return <option value={JSON.stringify(h)}>{h.hospitalName}</option>
+                        })
+                    }
+                </select>
+            )
+        }
+        {
+            bedTypes.length>0 && (
+                <select>
+                    {bedTypes.map((bt)=>{
+                        return <option value={JSON.stringify(bt)}>{bt.bedType}</option>
+                    })}
+                </select>
+                
+            )
+        }
+    </div>
+  )
+}
+
+export default AddBed
