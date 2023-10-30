c7cd65c9a0c3a9093acca74d1baaa4e25bff61b1
Manasa-dev503 <198a1a0503manasa@gmail.com>
2023-10-30 12:21:15 +0530

    class to className and removed unnecessary code

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from '../firebase';
@@ -24,7 +24,7 @@ function Header() {
         var admns = data.filter((mail)=>{
           return (mail===userDetails.mailId)
         })
-        if(admns.length==0){
+        if(admns.length===0){
           dispatch(changeUser({isadmin:false,user:userDetails}))
         }
         if(admns.length>0){
@@ -41,31 +41,31 @@ function Header() {
 
   return (
     <div>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-            <div class="container-fluid">
-                <Link class="navbar-brand">HSP</Link>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-danger">
+            <div className="container-fluid">
+                <Link className="navbar-brand">HSP</Link>
                 {
                   isAdmin &&
-                  <div style={{marginLeft:"350px"}} class="navbar-nav">
-                    <Link class="nav-link" to="/admindashboard">Admin DashBoard</Link>
+                  <div style={{marginLeft:"350px"}} className="navbar-nav">
+                    <Link className="nav-link" to="/admindashboard">Admin DashBoard</Link>
                   </div>
                 }
                 {
                   !user &&
                   <button className='btn' onClick={()=>{login()}}>
                     <span>Login</span>&nbsp;
-                    <i class="bi bi-box-arrow-in-right"></i>
+                    <i className="bi bi-box-arrow-in-right"></i>
                   </button>
                 }
                     {
                         user &&
                         <div className="dropdown">
                             <span className='text-white' >Howdy, {user.name}</span>
-                            <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                <img style={{"width":"25px","height":"25px","border-radius":"50%"}} src={user.image} />
+                            <button className="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                <img style={{"width":"25px","height":"25px","border-radius":"50%"}} src={user.image} alt="" />
                             </button>
                             <ul className="dropdown-menu profile-menu" aria-labelledby="dropdownMenuButton1">
-                              <li><button class="dropdown-item" onClick={()=>{logOut()}}>Logout</button></li>
+                              <li><button className="dropdown-item" onClick={()=>{logOut()}}>Logout</button></li>
                             </ul>
                         </div>
                     }
